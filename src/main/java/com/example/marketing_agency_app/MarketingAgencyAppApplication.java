bb3c355538706ea65eba50e70983a0a1cef24ee7
diekinari <91372629+diekinari@users.noreply.github.com>
2025-03-24 11:23:56 +0300

    init

---
+++ src/main/java/com/example/marketing_agency_app/MarketingAgencyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketing_agency_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingAgencyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingAgencyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marketing_agency_app/MarketingAgencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketing_agency_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingAgencyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
